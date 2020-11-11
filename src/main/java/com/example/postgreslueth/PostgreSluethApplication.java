b3a7a0146a3440da9e689796212dd23fd0dddd5e
machih <machih@vmware.com>
2020-11-11 09:36:10 +0900

    Format

--- src/main/java/com/example/postgreslueth/PostgreSluethApplication.java
+++ src/main/java/com/example/postgreslueth/PostgreSluethApplication.java
@@ -1,6 +1,5 @@
 package com.example.postgreslueth;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import brave.Span;
 import brave.Tracer;
-
 
 import java.util.List;
 import java.util.Map;
@@ -23,41 +21,17 @@ public class PostgreSluethApplication {
 		SpringApplication.run(PostgreSluethApplication.class, args);
 	}
 
-
 	@Autowired
 	JdbcTemplate jdbcTemplate;
-	
+
 	@Autowired
 	Tracer tracer;
-	
+
 	@RequestMapping("/car")
 	public String index() {
 		String list = performSql("select * from car");
 		return list;
 	}
-	
-	
-/*
-	Tracer createWavefrontTracer(String application, String service) throws IOException {
-		// Step 1. Create ApplicationTags.
-		ApplicationTags applicationTags = new ApplicationTags.Builder(application, service).build();
-
-		// Step 2. Create a WavefrontSender and configure it to send data via a
-		// Wavefront proxy. WavefrontClientFactory is used to send data to multiple
-		// Wavefront services, such as the metrics port and the distribution port.
-		// Assume you have installed and started the proxy on <proxyHostname>.
-		WavefrontClientFactory wavefrontClientFactory = new WavefrontClientFactory();
-		wavefrontClientFactory.addClient("https://d2702db1-483b-431f-83cc-c6ca0f5262d1@wavefront.surf");
-		WavefrontSender wavefrontSender = wavefrontClientFactory.getClient();
-
-		// Step 3. Create a WavefrontSpanReporter for reporting trace data that
-		// originates on <sourceName>.
-		Reporter wfSpanReporter = new WavefrontSpanReporter.Builder().withSource("test").build(wavefrontSender);
-
-		// Step 4. Create the WavefrontTracer.
-		return new WavefrontTracer.Builder(wfSpanReporter, applicationTags).build();
-	}*/
-	
 
 	String performSql(String SQL) {
 		Span newSpan = this.tracer.nextSpan().name("postgres").start();
@@ -65,11 +39,11 @@ public class PostgreSluethApplication {
 			newSpan.tag("component", "java-jdbc");
 			newSpan.tag("span.kind", "client");
 			newSpan.tag("db.type", "postgresql");
-			newSpan.tag("db.instance", "localDB");		
-		    List<Map<String, Object>> list;
-		    list = jdbcTemplate.queryForList(SQL);
-		    return  list.toString();
-		}finally {			
+			newSpan.tag("db.instance", "localDB");
+			List<Map<String, Object>> list;
+			list = jdbcTemplate.queryForList(SQL);
+			return list.toString();
+		} finally {
 			newSpan.finish();
 		}
 	}
